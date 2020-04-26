b16796577f7cf9305dd9a2cfd83889adcceb14fa
Walter Nam <49884140+wnam98@users.noreply.github.com>
2020-04-26 18:19:55 +0900

    added script and fixed directories

--- script.py
+++ script.py
@@ -1,12 +1,40 @@
 #Author: Walter Nam
-from numpy import *
+import numpy as np
 
-class Data:
+#first gauss jordan to find the RREF
 
-    def __init__(self, A, x, b):
-        self.A = A
-        self.x = x
-        self.b = b
+def gauss_jrdn(a, b):
+    a = np.array(a, float)
+    b = np.array(b, float)
+    n = len(b)
 
-    def elimination(self):
-        pass
+    #main loop
+    for k in range(n):
+        #pivoting
+        if np.fabs(a[k,k]) < 1.0e-12:
+            for i in range(k + 1, n):
+                if np.fabs(a[i, k]) > np.fabs(a[k, k]):
+                    for j in range(k, n):
+                        a[k, j], a[i, j] = a[i, j], a[k,j]
+                    b[k], b[i] = b[i], b[k]
+                    break
+        #Division of the pivot row
+        pivot = a[k, k]
+        for j in range(k, n):
+            a[k, j] /= pivot
+        b[k] /= pivot
+        #Elimination Loop
+        for i in range(n):
+            if i == k or a[i,k] == 0: continue
+            factor = a[i,k]
+            for j in range(k, n):
+                a[i,j] -= factor * a[k, j]
+            b[i] -= factor * b[k]
+    return b,a
+
+a = [[1,-1,2,-1], [2,-2, 3, -3], [1,1,1,0], [1, -1, 4, -3]]
+b = [-8, -20, -2, 4]
+
+X,A = gauss_jrdn(a, b)
+print(X)
+print(A)
---
+++ scrpt/script.py
@@ -0,0 +1,12 @@
+#Author: Walter Nam
+from numpy import *
+
+class Data:
+
+    def __init__(self, A, x, b):
+        self.A = A
+        self.x = x
+        self.b = b
+
+    def elimination(self):
+        pass
