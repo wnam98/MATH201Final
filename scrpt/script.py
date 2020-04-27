768412a7fc10563b917e632f9a44644b7ce5a71e
Walter Nam <49884140+wnam98@users.noreply.github.com>
2020-04-27 14:28:52 +0900

    scrpt

--- scrpt/script.py
+++ scrpt/script.py
@@ -1,12 +1,10 @@
 #Author: Walter Nam
-from numpy import *
+import numpy as np
 
-class Data:
+a = np.array([1,-1,2,-1],[2,-2,3,-3],[1,1,1,0],[1,-1,4,3], float)
+b = np.array([-8, -20, -2, 4], float)
+n = len(b)
+x = np.zeros(n, float)
 
-    def __init__(self, A, x, b):
-        self.A = A
-        self.x = x
-        self.b = b
-
-    def elimination(self):
-        pass
+#Elimination
+for k in range(n - 1):
